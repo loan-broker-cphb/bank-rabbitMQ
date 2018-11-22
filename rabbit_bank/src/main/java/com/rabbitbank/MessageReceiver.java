e1178e7b4a3a4b1d613067abefb0bd2e26de9eed
Mikkel Ziemer <mzhjensen@gmail.com>
2018-11-22 14:34:30 +0100

    Fixed setreply

--- rabbit_bank/src/main/java/com/rabbitbank/MessageReceiver.java
+++ rabbit_bank/src/main/java/com/rabbitbank/MessageReceiver.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -21,7 +22,7 @@ public class MessageReceiver {
     private RabbitTemplate rabbitTemplate;
 
     @Autowired
-    private MessageConverter messageConverter;
+    private MessageConverter jsonMessageConverter;
 
     @Autowired
     RabbitAdmin rabbitAdmin;
@@ -29,8 +30,9 @@ public class MessageReceiver {
     public MessageReceiver() {
     }
 
+    @RabbitListener(queues = Application.queueName)
     public void handleMessage(Message message) {
-        MessageRequest quoteRequest = (MessageRequest) messageConverter.fromMessage(message);
+        MessageRequest quoteRequest = (MessageRequest) jsonMessageConverter.fromMessage(message);
         String replyTo = message.getMessageProperties().getReplyTo();
 
         // Calculate Loan intrest rate
@@ -38,7 +40,7 @@ public class MessageReceiver {
 
         Properties prop = rabbitAdmin.getQueueProperties(replyTo);
         if (prop == null) {
-            Queue queue = new Queue(replyTo, true, false, true);
+            Queue queue = new Queue(replyTo, true, false, false);
             rabbitAdmin.declareQueue(queue);
             Binding binding = new Binding(replyTo, Binding.DestinationType.QUEUE, Application.exchange, replyTo, null);
             rabbitAdmin.declareBinding(binding);
@@ -50,8 +52,9 @@ public class MessageReceiver {
         responce.setSsn(quoteRequest.getSsn());
 
         // Convert response to Message
-        Message responseMessage = messageConverter.toMessage(responce, properties);
+        Message responseMessage = jsonMessageConverter.toMessage(responce, properties);
 //        rabbitTemplate.setQueue(replyTo);
+        System.out.println(responce.ssn);
         rabbitTemplate.send(replyTo, responseMessage);
 
 //        below goes in Translator
@@ -60,52 +63,6 @@ public class MessageReceiver {
 //            m.getMessageProperties().setReplyTo("g4.rabbit.reply-to");
 //            return m;
 //        });
-    }
-
-    private class MessageRequest{
-
-        private String ssn;
-
-        private int creditScore;
-
-        private double loanAmount;
-
-        private int loanDuration;
-
-        public MessageRequest() {
-        }
-
-        public String getSsn() {
-            return ssn;
-        }
-
-        public void setSsn(String ssn) {
-            this.ssn = ssn;
-        }
-
-        public int getCreditScore() {
-            return creditScore;
-        }
-
-        public void setCreditScore(int creditScore) {
-            this.creditScore = creditScore;
-        }
-
-        public double getLoanAmount() {
-            return loanAmount;
-        }
-
-        public void setLoanAmount(double loanAmount) {
-            this.loanAmount = loanAmount;
-        }
-
-        public int getLoanDuration() {
-            return loanDuration;
-        }
-
-        public void setLoanDuration(int loanDuration) {
-            this.loanDuration = loanDuration;
-        }
     }
 
     private class MessageResponce{
